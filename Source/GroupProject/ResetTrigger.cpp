21e9950893c18fce22edbc61aaa12c4efd0713ac
Ivar1999 <249314@stud.inn.no>
2021-04-09 15:32:10 +0200

    Reset level & boundries

---
+++ Source/GroupProject/ResetTrigger.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ResetTrigger.h"
+#include "Engine/BrushShape.h"
+#include "Kismet/GameplayStatics.h"
+AResetTrigger::AResetTrigger()
+{
+
+
+	OnActorBeginOverlap.AddDynamic(this, &AResetTrigger::OnOverlapBegin);
+	OnActorEndOverlap.AddDynamic(this, &AResetTrigger::OnOverlapEnd);
+
+}
+
+
+
+void AResetTrigger::BeginPlay()
+{
+	Super::BeginPlay();
+
+	
+
+}
+
+void AResetTrigger::OnOverlapBegin(AActor* OverlappedActor, AActor* OtherActor)
+{
+	if (OtherActor && (OtherActor !=this))
+	{
+		RestartingLevel();
+	}
+}
+
+void AResetTrigger::OnOverlapEnd(AActor* OverlappedActor, AActor* OtherActor)
+{
+	if (OtherActor && (OtherActor != this))
+	{
+		
+	}
+}
+void AResetTrigger::RestartingLevel()
+{
+	UGameplayStatics::OpenLevel(this, *GetWorld()->GetName(), false);
+}
